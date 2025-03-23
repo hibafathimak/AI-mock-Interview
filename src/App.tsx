eb666e65f33506b6a0dae1da4bcf48a6f7bf3b48
hibaaahhh <hibakalathil02@gmail.com>
2025-03-23 16:50:37 +0530

    initalization

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Button variant="default" >Click Me</Button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
