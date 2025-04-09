93a54b2df8bc081b1387cec19cf4ef55dc618502
hibaaahhh <hibakalathil02@gmail.com>
2025-04-09 13:38:14 +0530

    MockAI Final Commit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2HSLWIEhUbAU8xNRYShNbQDrgu11SGD0",
-  authDomain: "mock-interview-ai-cbb57.firebaseapp.com",
-  projectId: "mock-interview-ai-cbb57",
-  storageBucket: "mock-interview-ai-cbb57.firebasestorage.app",
-  messagingSenderId: "9411874316",
-  appId: "1:9411874316:web:09fd036fa509208ea1e829",
-  measurementId: "G-PEYG1GBEWJ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app)
+const db = getFirestore(app);
 const Googleprovider = new GoogleAuthProvider();
 
-export { auth, Googleprovider,db }
+export { auth, Googleprovider, db };
